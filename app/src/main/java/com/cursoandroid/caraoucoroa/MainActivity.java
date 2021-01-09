6fa264da19a2f6d894dd194fb35ab27346e805e7
Kaic Carvalho <kaic15_fernandes@hotmail.com>
2021-01-09 16:46:16 -0300

    FInalização da navegação e conclusão do app.

--- app/src/main/java/com/cursoandroid/caraoucoroa/MainActivity.java
+++ app/src/main/java/com/cursoandroid/caraoucoroa/MainActivity.java
@@ -2,13 +2,40 @@ package com.cursoandroid.caraoucoroa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonJogar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonJogar = findViewById(R.id.buttonJogar);
+
+        buttonJogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Instanciar a intenção
+                Intent intent = new Intent(getApplicationContext(), ResultadoActivity.class);
+
+                //Passar dados RANDOM
+                int numero = new Random().nextInt(2);
+                intent.putExtra("numero", numero);
+
+                //Start Activity com a intenção instanciada
+                startActivity(intent);
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/cursoandroid/caraoucoroa/ResultadoActivity.java
+++ app/src/main/java/com/cursoandroid/caraoucoroa/ResultadoActivity.java
@@ -2,13 +2,44 @@ package com.cursoandroid.caraoucoroa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class ResultadoActivity extends AppCompatActivity {
+
+    private ImageView imageResultado;
+    private Button buttonVoltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
+
+        imageResultado = findViewById(R.id.imageResultado);
+        buttonVoltar = findViewById(R.id.buttonVoltar);
+
+        //Recuperar dados
+        Bundle dados = getIntent().getExtras();
+        int numero = dados.getInt("numero");
+
+        //0 = cara, 1 coroa
+        if ( numero == 0){
+            imageResultado.setImageResource(R.drawable.moeda_cara);
+        }else{
+            imageResultado.setImageResource(R.drawable.moeda_coroa);
+        }
+
+        buttonVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Encerrar a Activity
+                finish();
+
+            }
+        });
+
     }
 }
